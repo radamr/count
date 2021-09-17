c1476615098ef5c42cc7f8cd371367a7c2ba8bb7
radamr <rybicki-adam@wp.pl>
2021-09-17 18:57:24 +0200

    count to 30

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    count += 1
+    basic.showNumber(count)
+})
+input.onButtonPressed(Button.B, function () {
+    count = 0
+    for (let index = 0; index < 30; index++) {
+        count += 1
+        basic.showNumber(count)
+    }
+})
+let count = 0
+count = 0
